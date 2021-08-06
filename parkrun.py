9a46f601313fe986d81b9392b830ab8f2b73661f
Смирнов Сергей <54954443+brannvann@users.noreply.github.com>
2021-08-07 02:00:00 +0300

    Update parkrun.py
    Добавил печать всех результатов забега

--- parkrun.py
+++ parkrun.py
@@ -87,17 +87,7 @@ def all_countries():
 
 def park_history(country, park):
 	parkurl = country_url(country) + "/" + park + '/results/eventhistory/'
-	isFile=False
-	history_page = ''
-	if isFile:
-		tmpfile = open(r'eventhistory.html', 'r')
-		history_page = tmpfile.read()
-	else:
-		history_page = read_url(parkurl)
-		tmpfile = open(r'eventhistory.html', 'w')
-		tmpfile.write(history_page)
-		tmpfile.close()
-	#print(history_page)
+	history_page = read_url(parkurl)
 	bs = BeautifulSoup(history_page, 'html.parser')
 	history_table = bs.tbody
 	events = []
@@ -111,9 +101,9 @@ def park_history(country, park):
 			event_date = datefmt[2]+datefmt[1]+datefmt[0] 
 			runners = row['data-finishers']
 			volunteers = row['data-volunteers']
-			#print(event_date,'\t', park, '\t', event_num, '\t', runners, '\t', volunteers)
 			events.append((event_date, event_num, runners, volunteers))
 	return events
+	
 	
 def country_history(country):
 	events = []
@@ -124,6 +114,7 @@ def country_history(country):
 		if len(eventhistory) > 0 : 
 			events.append((parkname, len(eventhistory), eventhistory[-1][0], eventhistory[0][0]))
 	return events
+	
 	
 def parkrun_results(country, park, num):
 	results = []
@@ -147,12 +138,12 @@ def parkrun_results(country, park, num):
 			age_group = row['data-agegroup']
 			age_grade = row['data-agegrade']
 			best_result = row['data-achievement']
-			if ( len(name)==0 ) or (name.lower()=='неизвестный'):
-				print(event_date, park, str(num), pos, name, sep = '\t')
+			data_runs = row['data-runs']
+			if ( len(data_runs)==0 ) or (data_runs=='0'):
+				results.append((event_date, park, str(num), pos, name))
 			else:
-				runner_id='A111111'
+				runner_id=''
 				refs=row.findAll('a')
-				#print(refs)
 				for ref in refs:
 					refhref = ref['href']
 					if 'athleteNumber=' in refhref:
@@ -160,31 +151,36 @@ def parkrun_results(country, park, num):
 						break
 				fields = row.findAll('td')
 				time_str = ''
-				gender_str = 'X'
-				gender_pos = 'X'
+				gender_str = ''
+				gender_pos = ''
 				for field in fields:
 					if 'Results-table-td--time' in field['class']:
-						time_str = field.contents[0].contents[0]
+						if field.contents[0] and field.contents[0].contents:
+							time_str = field.contents[0].contents[0]
+							timebuf = time_str.split(':')
+							if len(timebuf) == 3:
+								minutes = int(timebuf[0])*60 + int(timebuf[1])
+								time_str = str(minutes)+':'+timebuf[2]
 					if 'Results-table-td--F' in field['class']:
 						gender_str = 'F'
 						gender_pos=field.findAll('div')[1].contents[0]
-						#print(ff[1].contents[0])
 					if 'Results-table-td--M' in field['class']:
 						gender_str = 'M' 
 						gender_pos=field.findAll('div')[1].contents[0]
 				if best_result == '':
 					best_result = row.findAll('div')[-1].contents[-1]
-						
-				#print(event_date, park, str(num), pos, 'A'+runner_id, name, time_str, gender_str, gender_pos, age_group, age_grade, best_result, sep = '\t')
 				results.append((event_date, park, str(num), pos, 'A'+runner_id, name, time_str, gender_str, gender_pos, age_group, age_grade, best_result))
 	return results
-#parkrun_results			
-			
-		
-		
 	
 	
-
+def parkrun_results_by_date(country, park, event_date):
+	results = []
+	events = park_history(country, park)
+	for info in events:
+		if info[0] == event_date:
+			results = parkrun_results(country, park, info[1])
+			break
+	return results
 
 def print_country_history(country):
 	events = country_history(country)
@@ -207,16 +203,41 @@ def print_parkrun_results(country, park, num):
 			print(r[0],r[1],r[2],r[3],r[4],r[5],r[6],r[7],r[8],r[9], r[10], r[11], sep='\t')
 
 
-#print_park_history('ie', 'thegrandcanalway')
-#print_country_history('my')
-#print_park_history('ru', 'bitsa')
+def print_parkrun_results_by_date(country, park, event_date):
+	results = parkrun_results_by_date(country, park, event_date)
+	for r in results:
+		if len(r) == 5: 
+			print(r[0],r[1],r[2],r[3],r[4],sep='\t')
+		if len(r) == 12: 
+			print(r[0],r[1],r[2],r[3],r[4],r[5],r[6],r[7],r[8],r[9], r[10], r[11], sep='\t')
+			
+def save_parkrun_results(country, park):
+	events = park_history(country, park)
+	events_num = len(events)
+	dir_name = country
+	filename = park + '_results.txt'
+	file_path = os.path.join(dir_name, filename)
+	ofs = open(file_path, 'a+')
+	while events_num > 0:
+		test_str = park + '\t' + str(events_num)
+		#results = parkrun_results(country, park, event_num)
+		print_parkrun_results(country, park, events_num)
+		events_num = events_num - 1 
 
-print_parkrun_results('ru', 'korolev', 66)
+#print_park_history('ru', 'korolev')
+#print_parkrun_results_by_date('ru', 'korolev', '20181020')
+
+#print_parkrun_results('ru', 'bitsa', 74)
+#print_parkrun_results('uk', 'Bushy', 206)
+
+save_parkrun_results('ru', 'purnavolok')
+
+#print_country_history('ru')
+
+#print_park_history('jp', 'odakaryokuchi')
+
+#print_parkrun_results('jp', 'odakaryokuchi', 14)
 	
-if False:
-	parks = get_all_parks('pl')
-	for park in parks:
-		print(park)
 
 
 
